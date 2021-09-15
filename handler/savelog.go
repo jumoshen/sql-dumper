5fe34f2550bb5925215a1de629d734c0f8a437e1
liang hui <lianghui@wdcloud.cc>
2021-09-15 09:01:22 +0800

    debug log

--- handler/savelog.go
+++ handler/savelog.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -12,6 +13,9 @@ import (
 func SaveVisitLog(ctx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		//todo 使用百度地图获取位置信息
+		fmt.Printf("header:%#v\n", r.Header)
+
+		//todo https://api.map.baidu.com/location/ip?ak=vaXwBb4abtGvukEOOLA19Qltx3Ndua4c&ip=39.155.169.2&coor=bd09ll
 		result, err := model.ManagerDao.SaveVisitLog(r.Context(), &model.VisitLogs{
 			Url:         "https://www.baidu.com",
 			Ip:          utils.ClientIP(r),
